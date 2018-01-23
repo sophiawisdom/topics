2d00e7988ca702613517cabd4a43b2b19129ef86
Will Weinstein <will@weinstein-nyc.com>
2018-01-23 14:14:55 -0500

    cleaning up code

--- peripheral.swift
+++ peripheral.swift
@@ -1,31 +1,36 @@
+//
+//  main.swift
+//  testBluetooth
+//
+//  Created by Will Weinstein on 1/8/18.
+//  Copyright © 2018 Will Weinstein. All rights reserved.
+//
 
 import Foundation
 import CoreBluetooth
 
 class Wow: NSObject, CBPeripheralManagerDelegate {
     var peripheralManager: CBPeripheralManager!
-    var peripheral: CBPeripheral!
     
-    func init() {
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        let advertisementData = [CBAdvertisementDataLocalNameKey: "Test Device"]
-        peripheralManager.startAdvertising(advertisementData)
-        
+    override init() {
+        super.init()
         
         let properties: CBCharacteristicProperties = [.notify, .read, .write]
         let permissions: CBAttributePermissions = [.readable, .writeable]
+        let standInUUID = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+        let advertisementData = [CBAdvertisementDataLocalNameKey: "Test Device"]
         
-        let blah = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
-        let someChar = CBMutableCharacteristic(type:blah, properties:properties
-            , value:nil, permissions:permissions)
-        let someService = CBMutableService(type:CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de"), primary:true)
-        someService.characteristics = [someChar]
         
+        
+        let someCharacteristic = CBMutableCharacteristic(type: standInUUID, properties: properties, value: nil, permissions: permissions)
+        let someService = CBMutableService(type:standInUUID, primary:true)
+        someService.characteristics = [someCharacteristic]
+        
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         peripheralManager.add(someService)
+        peripheralManager.startAdvertising(advertisementData)
         
-        
-        
-        print("Hello, World!")
+        print("Ready to go")
 
     }
 
@@ -45,9 +50,11 @@ class Wow: NSObject, CBPeripheralManagerDelegate {
     }
 
 }
+
+
 var thingy = Wow()
-thingy.start()
 
 while(true) {
     
 }
+
