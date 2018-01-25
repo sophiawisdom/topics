5505b8e7c8b8b2cf0e50c8f3903c98b2c71e4765
Sophia Wisdom <wisdomw18@computer.local>
2018-01-25 14:32:34 -0500

    Added changes or something

--- central.swift
+++ central.swift
@@ -2,15 +2,26 @@
 import Foundation
 import CoreBluetooth
 
-sdfg
 class CentralBoye: NSObject, CBCentralManagerDelegate {
     var centralManager: CBCentralManager!
-    var peripheral: CBPeripheral
+    var peripheral: CBPeripheral!
     
-    init() {
-        let blah = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate:self, queue:nil)
+        let service_uuid = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+        centralManager.scanForPeripherals(withServices:nil, options: nil)
     }
-    func centralManagerDidUpdateState(central: CBCentralManager) {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print("state: \(central.state)")
     }
+    func centralManager(central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any],
+                        RSSI: NSNumber){ // Receives result of peripheral scan
+        print("peripheral: \(peripheral)")
+    }
 }
+
+var cls = CentralBoye()
