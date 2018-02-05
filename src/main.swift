7c7a20a9c11ba03cf3191ffce1d71aa56937aa62
Will Weinstein <will@weinstein-nyc.com>
2018-02-05 11:17:55 -0500

    peripheral works now

--- src/main.swift
+++ src/main.swift
@@ -1,8 +1,50 @@
 import Foundation
+import CoreBluetooth
 
-var periph_man = PeripheralMan()
-var central_man = CentralMan()
+let periph_man = PeripheralMan()
+usleep(500000)
+var queue: DispatchQueue!
+if #available(OSX 10.10, *) {
+    queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
+} else {
+    print("Update your OS??")
+}
+periph_man.peripheralManager = CBPeripheralManager(delegate: periph_man, queue: queue)
+
+let properties: CBCharacteristicProperties = [.notify, .read, .write]
+let permissions: CBAttributePermissions = [.readable, .writeable]
+let standInUUID = CBUUID(string: "00001801-0000-1000-8000-00805f9b34fb") //these UUIDS probably need to be changed
+let serviceUUID = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+let someCharacteristic = CBMutableCharacteristic(type: standInUUID, properties: properties, value: nil, permissions: permissions)
+let someService = CBMutableService(type:serviceUUID, primary:true)
+
+someService.characteristics = [someCharacteristic]
+let advertisementData: [String : Any] = [CBAdvertisementDataLocalNameKey : "JasonChase", CBAdvertisementDataServiceUUIDsKey : ["a495ff20-c5b1-4b44-b512-1370f02d74de"]]// probably the right format, thank apple for their definitely helpful documentation
+
+
+
+
 
 while (true){
-    usleep(1000)
+    usleep(1000000)
+    switch(periph_man.peripheralManager.state) { //just prints out what state the peripheral is in, if it's not on something is probably going wrong
+    case .poweredOn:
+        print("on")
+        print("advertising: \(periph_man.peripheralManager.isAdvertising)")
+        if(!periph_man.peripheralManager.isAdvertising) {
+            periph_man.peripheralManager.add(someService)
+            periph_man.peripheralManager.startAdvertising(advertisementData)
+        }
+    case .poweredOff:
+        print("off")
+    case .resetting:
+        print("resetting")
+    case .unauthorized:
+        print("unauthorized")
+    case .unknown:
+        print("unknown")
+    default:
+        print("spooky")
+    }
 }
+
--- src/peripheral.swift
+++ src/peripheral.swift
@@ -6,41 +6,32 @@ import CoreBluetooth
 class PeripheralMan: NSObject, CBPeripheralManagerDelegate {
     var peripheralManager: CBPeripheralManager!
     
-    override init() {
-        super.init()
-        
-        let properties: CBCharacteristicProperties = [.notify, .read, .write]
-        let permissions: CBAttributePermissions = [.readable, .writeable]
-        let standInUUID = CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
-        let advertisementData = [CBAdvertisementDataLocalNameKey: "Test Device"]
-        
-        
-        
-        let someCharacteristic = CBMutableCharacteristic(type: standInUUID, properties: properties, value: nil, permissions: permissions)
-        let someService = CBMutableService(type:standInUUID, primary:true)
-        someService.characteristics = [someCharacteristic]
-        
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        peripheralManager.add(someService)
-        peripheralManager.startAdvertising(advertisementData)
-        
-        print("Peripheral should have started advertising")
-
-    }
+    
 
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager)
     {
-        print("state: \(peripheral.state)")
+        print("peripheralManager state has changed. This is probably good")
     }
+    
 
-
-    func peripheralManagerDidStartAdvertising(_ error: NSError?)
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager,
+                                             error: Error?)
     {
         if let error = error {
             print("Failed… error: \(error)")
             return
         }
-        print("Succeeded!")
+        print("Advertising Begins!")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager, didAdd service: CBService, error: NSError?)
+    {
+        if let error = error {
+            print("error: \(error)")
+            return
+        }
+
+        print("service: \(service)")
     }
 
 }
