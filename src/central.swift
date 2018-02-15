48eea2a3f329aef07ca72642362ad7bd0a08bdea
Sophia Wisdom <wisdomw18@computer.local>
2018-02-14 20:18:43 -0500

    changed central logic

--- src/central.swift
+++ src/central.swift
@@ -19,37 +19,58 @@ class CentralMan: NSObject, CBCentralManagerDelegate {
         print("Central Manager state has changed. This is probably good.")
     }
     func centralManager(_: CBCentralManager, didDiscover: CBPeripheral, advertisementData: [String : Any], rssi: NSNumber){ // Receives result of peripheral scan
-        let UUID = advertisementData[CBAdvertisementDataServiceUUIDsKey] as! String
+        print("diddiscover called")
+        if let BUUID = advertisementData[CBAdvertisementDataServiceUUIDsKey] { // if this key exists
+            switch BUUID {
+            case "hello" as String:
+                print("Got em")
+            default:
+                print("ServiceUUIDS other than 'hello'")
+            }
+        }
+        else if let name = advertisementData[CBAdvertisementDataLocalNameKey] {
+            switch name {
+            case "JasonChase" as String:
+                print("Found Local Name Jason Chase")
+            default:
+                print("Found peripheral without UUIDS and with name but name not Jason Chase. Name was bad")
+            }
+        }
         
-        print("Discovered peripheral with UUID \(UUID)")
-        if (UUID == "hello"){
-            print("Attempting to connect to known UUID")
-        }
-        else {
-            print("UUID unknown. No connection made")
-            return
-        }
-        centralManager.connect(didDiscover, options: nil)
         
         didDiscover.delegate = del
+        
+        centralManager.connect(didDiscover, options: nil)
+        
+        
         
         
         knownPeripherals.append(didDiscover)
     }
     
     func centralManager(_ central: CBCentralManager, didConnect: CBPeripheral) {
-        print("connected")
-        
+        print("Connected to peripheral")
+        didConnect.delegate = del
         didConnect.discoverServices(nil)
-        print("Started to discover services")
-        sleep(500000)
+        var num_waits = 0
+        while didConnect.services == nil{
+            usleep(1000)
+            num_waits += 1
+            if (num_waits > 1 && (num_waits%1000 == 0)){
+                print("Waiting for didConnect.services to be non-nil")
+            }
+        }
         var have_message_service = false
-        for service in peripheral.services! {
+        print("Peripheral has \(didConnect.services!.count) services")
+        for service in didConnect.services! {
             print("Service discovered on peripheral: \(service)")
-            if (service == messageServiceUUID){
+            if (service.uuid == messageServiceUUID){
                 have_message_service = true
                 print("Found message service UUID on peripheral")
             }
+        }
+        if (have_message_service == true){
+            
         }
         
     }
@@ -79,6 +100,7 @@ class PeripheralDelegate: NSObject, CBCentralManagerDelegate, CBPeripheralDelega
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?)
     {
+        print("peripheral func")
         let services = peripheral.services
         print("Found \(services!.count) services! :\(services!) for peripheral \(peripheral)")
     }
--- src/main.swift
+++ src/main.swift
@@ -2,21 +2,25 @@ import Foundation
 import CoreBluetooth
 
 //let periph_man = PeripheralMan()
-//let central_man = CentralMan()
-let periphMan = PeripheralMan()
-start_advertising(periph_man:periphMan)
+let central_man = CentralMan()
+//let periphMan = PeripheralMan()
+//start_advertising(periph_man:periphMan)
+var queue: DispatchQueue!
+if #available(OSX 10.10, *) {
+    queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
+} else {
+    print("DispatchQueue not available on your version of MacOSX. Please update to 10.10 or greater.")
+}
 
+central_man.centralManager = CBCentralManager(delegate: central_man, queue: queue)
 
 let standInUUID = CBUUID(string: "0x1800")
 //var ids: [CBUUID] = [standInUUID]
-
-//if(central_man.centralManager.state == .poweredOn) {
-//    central_man.centralManager.scanForPeripherals(withServices: [standInUUID])
-//}
-//else {
-//    print("CentralManager state is not powered on. Perhaps your bluetooth is off.")
-//}
-
+while (!(central_man.centralManager.state == .poweredOn)){
+    usleep(1000)
+}
+print("Central Manager powered on")
+central_man.centralManager.scanForPeripherals(withServices:nil)
 while (true){
     usleep(100000)
     //print("huH")
